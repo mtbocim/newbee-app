bc9dbf28ddc429e897956a13df4c4a837f9d8857
michael-bocim <mtbocim@gmail.com>
2023-11-09 13:01:27 -0700

    Getting all data, setting up page filtering

--- app/api/job_postings/route.ts
+++ app/api/job_postings/route.ts
@@ -2,6 +2,6 @@ import prisma from '@/app/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
-    let results = await prisma.job_postings.findMany({ take: 10 })
+    let results = await prisma.job_postings.findMany()
     return NextResponse.json(results)
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,50 @@ import { useEffect, useState } from "react";
 
 // import styles from "./page.module.css";
 
+interface dataInterface {
+  id: number;
+  job_title: string;
+  job_url: string;
+  job_id: string;
+  job_scraped_date: string;
+  company_name: string;
+  job_description: string;
+  json_response?: {
+      apply?: string;
+      salary?: string;
+      location?: string;
+      department?: string;
+      tech_stack?: string[];
+  };
+}
+
 export default function Home() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState<Array<dataInterface>>([]);
+  const [techStackTags, setTechStackTags] = useState<Array<{ value: string; label: string }>>([]);
+  const [locations, setLocations] = useState<Array<{ value: string; label: string }>>([]);
 
   useEffect(() => {
     async function getProjectsData() {
       const response = await fetch(`/api/job_postings`);
+
       const data = await response.json();
+      const techStacks = new Set<string>();
+      const jobLocations = new Set<string>();
+
+      for(const item of data){
+          if(item.json_response.tech_stack){
+            for(const tech of item.json_response.tech_stack)
+            techStacks.add(tech)
+          }
+          item.json_response.location && jobLocations.add(item.json_response.location)
+        }
+
+      setTechStackTags(() =>
+        Array.from(techStacks, (value) => ({ value: value, label: value }))
+      );
+      setLocations(() =>
+        Array.from(jobLocations, (value) => ({ value: value, label: value }))
+      );
       setData(data);
     }
     getProjectsData();
@@ -20,13 +57,16 @@ export default function Home() {
   return (
     <main>
       <div>
-    {/* <main className={styles.main}>
+        {/* <main className={styles.main}>
       <div className={styles.description}> */}
         Hello!
         {data.map((item, i) => {
           return (
             <div key={i} className="parent">
-              <div>{item.job_title} --- {item.company_name} <a href={item.job_url}>Apply</a></div>
+              <div>
+                {item.job_title} --- {item.company_name}{" "}
+                <a href={item.job_url}>Apply</a>
+              </div>
             </div>
           );
         })}
