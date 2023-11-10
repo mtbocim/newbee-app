eb6ec92152558d7f95f06357a39ed6c3962a9846
michael-bocim <mtbocim@gmail.com>
2023-11-10 15:53:33 -0700

    Limiting query set from DB

--- app/api/job_postings/route.ts
+++ app/api/job_postings/route.ts
@@ -2,6 +2,11 @@ import prisma from '@/app/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function GET() {
-    let results = await prisma.job_postings.findMany()
+    let results = await prisma.job_postings.findMany({where:{
+        json_response:{
+            path: ['apply'],
+            equals:'True'
+        }
+    }})
     return NextResponse.json(results)
 }
