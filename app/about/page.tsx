c590d7745fab362143ba2445213bd5922a9b7900
michael-bocim <mtbocim@gmail.com>
2023-11-11 15:49:40 -0700

    Basic about page data population

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,32 @@
-export default function About(){
-    return(
-        <div>
-            About page
+import Image from 'next/image'
+
+const contributors = [
+  "camrandev",
+  "CodingHobo",
+  "hbnnguyen",
+  "MGHermanMancarella",
+  "mtbocim",
+  "stzheng716",
+];
+
+export default async function About() {
+    const contribData = []
+    for(const u of contributors){
+        const response = await fetch(`https://api.github.com/users/${u}`)
+        contribData.push(await response.json())
+    }
+
+    return( <div>
+        About page
+        {contribData.map(i=>{
+            return(
+                <div key={i.login}>
+                    {i.name}
+                    <Image src={i.avatar_url} alt={`${i.name}'s avatar`} width={100} height={100}/>    
+                </div>
+                
+            )
+        })}
         </div>
     )
 }
