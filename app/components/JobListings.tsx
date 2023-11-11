20b78004e8cc0347a93f3a6bf45c0e52ba771d66
michael-bocim <mtbocim@gmail.com>
2023-11-11 00:10:07 -0700

    'next/dynamic' import to deal with library component rendering

--- app/components/JobListings.tsx
+++ app/components/JobListings.tsx
@@ -1,115 +1,125 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from "react";
-import Select from "react-select";
 import JobDescription from "./JobDescription";
 import JobPostingsInterface from "../interfaces/JobPostingsInterface";
 import TagDataInterface from "../interfaces/TagDataInterface";
+import dynamic from 'next/dynamic'
 
 //This is solely for temp styling the 'Select' component, and until adding tailwind styling
 const selectStyles = {
-    control: (provided: any) => ({
-      ...provided,
-      width: "20vw",
-      minWidth: "400px",
-    }),
-    menuList: (provided: any) => ({
-      ...provided,
-      width: "20vw",
-      minWidth: "400px",
-    }),
-  };
+  control: (provided: any) => ({
+    ...provided,
+    width: "20vw",
+    minWidth: "400px",
+  }),
+  menuList: (provided: any) => ({
+    ...provided,
+    width: "20vw",
+    minWidth: "400px",
+  }),
+};
 
-export default function JobListings({jobListingData}:{jobListingData:Array<JobPostingsInterface>}){
+// This is to make the nextjs gods happy with the library, need to read
+// more about what exactly this is doing
+const Select = dynamic(
+  () => import('react-select'),
+  { ssr: false }
+)
 
-    const [data, setData] = useState<Array<JobPostingsInterface>>([]);
+export default function JobListings({
+  jobListingData,
+}: {
+  jobListingData: Array<JobPostingsInterface>;
+}) {
+  const [data, setData] = useState<Array<JobPostingsInterface>>([]);
 
-    const [techStackTags, setTechStackTags] = useState<Array<TagDataInterface>>(
-      []
-    );
-    const [locationTags, setLocationTags] = useState<Array<TagDataInterface>>([]);
+  const [techStackTags, setTechStackTags] = useState<Array<TagDataInterface>>(
+    []
+  );
+  const [locationTags, setLocationTags] = useState<Array<TagDataInterface>>([]);
 
-    const [selectedLocations, setSelectedLocations] = useState<
-      Array<TagDataInterface>
-    >([]);
-    const [selectedTech, setSelectedTech] = useState<Array<TagDataInterface>>([]);
+  const [selectedLocations, setSelectedLocations] = useState<
+    Array<TagDataInterface>
+  >([]);
+  const [selectedTech, setSelectedTech] = useState<Array<TagDataInterface>>([]);
 
-    useEffect(() => {
-      async function getProjectsData() {
-        const techStacks = new Set<string>();
-        const jobLocations = new Set<string>();
+  useEffect(() => {
+    async function getProjectsData() {
+      const techStacks = new Set<string>();
+      const jobLocations = new Set<string>();
 
-        for (const item of jobListingData) {
-          if (item.json_response?.tech_stack) {
-            for (const tech of item.json_response.tech_stack)
-              techStacks.add(tech);
-          }
-          item.json_response?.location &&
-            jobLocations.add(item.json_response.location);
+      for (const item of jobListingData) {
+        if (item.json_response?.tech_stack) {
+          for (const tech of item.json_response.tech_stack)
+            techStacks.add(tech);
         }
-
-        setTechStackTags(() =>
-          Array.from(techStacks, (value) => ({
-            value: value,
-            label: value,
-          }))
-        );
-
-        setLocationTags(() =>
-          Array.from(jobLocations, (value) => ({
-            value: value,
-            label: value,
-          }))
-        );
-        setData(() => jobListingData);
+        item.json_response?.location &&
+          jobLocations.add(item.json_response.location);
       }
-      getProjectsData();
-    }, [jobListingData]);
 
-    function handleSelectedTechTagsChange(
-      selectedValues: Array<TagDataInterface>
-    ) {
-      setSelectedTech(() => selectedValues);
-    }
+      setTechStackTags(() =>
+        Array.from(techStacks, (value) => ({
+          value: value,
+          label: value,
+        }))
+      );
 
-    function handleSelectedLocationTagsChange(
-      selectedValues: Array<TagDataInterface>
-    ) {
-      setSelectedLocations(() => selectedValues);
+      setLocationTags(() =>
+        Array.from(jobLocations, (value) => ({
+          value: value,
+          label: value,
+        }))
+      );
+      setData(() => jobListingData);
     }
+    getProjectsData();
+  }, [jobListingData]);
+
+  function handleSelectedTechTagsChange(
+    selectedValues: Array<TagDataInterface>
+  ) {
+    setSelectedTech(() => selectedValues);
+  }
+
+  function handleSelectedLocationTagsChange(
+    selectedValues: Array<TagDataInterface>
+  ) {
+    setSelectedLocations(() => selectedValues);
+  }
 
   return (
-    <main>
-      <div style={{ display: "flex" }}>
-        <Select
-          closeMenuOnSelect={false}
-          isMulti
-          options={techStackTags}
-          onChange={handleSelectedTechTagsChange}
-          value={selectedTech}
-          styles={selectStyles}
-        />
-        <Select
-          closeMenuOnSelect={false}
-          isMulti
-          options={locationTags}
-          onChange={handleSelectedLocationTagsChange}
-          value={selectedLocations}
-          styles={selectStyles}
-        />
-        <div style={{ display: "flex", flexFlow: "column" }}>
-          {selectedLocations.length !== 0 &&
-            selectedTech.length !== 0 &&
-            data.map((item: JobPostingsInterface, i) => (
-              <JobDescription
-                key={i}
-                jobDescription={item}
-                selectedTechTags={selectedTech}
-                selectedLocationTags={selectedLocations}
-              />
-            ))}
-        </div>
+    <div style={{ display: "flex" }}>
+      <Select
+        id="techStackTags"
+        closeMenuOnSelect={false}
+        isMulti
+        options={techStackTags}
+        onChange={handleSelectedTechTagsChange}
+        value={selectedTech}
+        styles={selectStyles}
+      />
+      <Select
+        id="locationTags"
+        closeMenuOnSelect={false}
+        isMulti
+        options={locationTags}
+        onChange={handleSelectedLocationTagsChange}
+        value={selectedLocations}
+        styles={selectStyles}
+      />
+      <div style={{ display: "flex", flexFlow: "column" }}>
+        {selectedLocations.length !== 0 &&
+          selectedTech.length !== 0 &&
+          data.map((item: JobPostingsInterface, i) => (
+            <JobDescription
+              key={i}
+              jobDescription={item}
+              selectedTechTags={selectedTech}
+              selectedLocationTags={selectedLocations}
+            />
+          ))}
       </div>
-    </main>
-  )
+    </div>
+  );
 }
