9e47d721fc270c0a4a5c973db258603416054799
Tanya Shylock <tourtanya@gmail.com>
2023-11-10 18:12:00 -0700

    add JobTable component, add logo to home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,12 @@
 import '@/app/styles/globals.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <>
-      <div className="navbar bg-primary">
+      <div className="navbar bg-accent">
         <div className="navbar-start">
+
           <div className="dropdown">
             <label tabIndex={0} className="btn btn-ghost btn-circle">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
@@ -28,8 +30,10 @@ export default function Home() {
           <span className="badge badge-xs badge-primary indicator-item"></span>
         </div>
       </button>
-    </div>
-  </div>
+        </div>
+      </div>
+      <Image src="/newBeeLogo3.png" alt="logo" width='380' height="100"></Image>
     </>
     );
 }
+
