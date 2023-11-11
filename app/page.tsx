8c5bd49a62e2917156fb0a29b621fa3e0b34a3a8
michael-bocim <mtbocim@gmail.com>
2023-11-11 13:00:13 -0700

    Home page skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,11 @@
-import JobListings from "./components/JobListings";
-import prisma from "./lib/prisma";
-import JobPostingsInterface from "./interfaces/JobPostingsInterface";
 
-export default async function Home() {
-  const results = process.env.DATABASE_URL && await prisma.job_postings.findMany({where:{
-    json_response:{
-        path: ['apply'],
-        equals:'True'
-    }
-}}) as Array<JobPostingsInterface>
 
-  return(
+export default function Home() {
+  
+  return (
     <div className="card w-96 bg-base-100 shadow-xl">
       Home page components here
-      <JobListings jobListingData={results}/>
+     
     </div>
   );
 }
-
-
