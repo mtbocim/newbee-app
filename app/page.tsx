d5669d7c66f01d9dbe9abd82e595aee06b8c1a4e
michael-bocim <mtbocim@gmail.com>
2023-11-15 13:58:14 -0700

    Text on screen baby!

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
-import Image from 'next/image';
-
-export default function Home() {
-  return (
-    <>
-      Hi
-    </>
-
-  );
-}
+import prisma from "@/app/lib/prisma";
+import JobPostingsInterface from "@/app/interfaces/JobPostingsInterface";
+import JobTable from "./components/JobTable";
+export default async function Home() {
+  const results = process.env.DATABASE_URL
+    ? ((await prisma.job_postings.findMany({
+        where: {
+          json_response: {
+            path: ["apply"],
+            equals: "True",
+          },
+        },
+      })) as Array<JobPostingsInterface>)
+    : [];
+    return (
+      // <>Hi</>
+      <JobTable descriptions={results}/>
+    );
+  }
