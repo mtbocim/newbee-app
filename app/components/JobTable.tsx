6787c2357a854968c818398ec07ead4a17e0f5f2
Tanya Shylock <tourtanya@gmail.com>
2023-11-15 15:16:10 -0700

    add styling to job table

--- app/components/JobTable.tsx
+++ app/components/JobTable.tsx
@@ -6,11 +6,11 @@ export default function JobTable({
   descriptions: Array<JobPostingsInterface>;
 }) {
   return (
-    <div className="JobTable overflow-x-auto grid-centered">
+    <div className="w-10/12 shadow overflow-x-auto grid-centered">
       <table className="table table-xs table-pin-rows">
         <thead>
-          <tr className='text-lg'>
-            <th >Job Title</th>
+          <tr className='font-bold text-xl'>
+            <th>Job Title</th>
             <th>Company</th>
             <th>Description Summary</th>
             <th>Tech Stack</th>
@@ -20,14 +20,14 @@ export default function JobTable({
         <tbody>
           {descriptions.map((item, i) => {
             return (
-              <tr key={i}>
-                <td style={{width:'10vw'}}>
+              <tr className="hover" key={i}>
+                <td className="w-1/12">
                   <a href={item.job_url} target="_blank">{item.job_title}</a>
-                </td>
-                <td>{item.company_name}</td>
-                <td style={{width:'35vw'}}>{item.job_description?.slice(0, 160)}</td>
-                <td style={{width:'35vw'}}>{item.json_response?.tech_stack?.join(", ")}</td>
-                <td>{item.json_response?.location}</td>
+                </td >
+                <td className="w-1/12">{item.company_name}</td>
+                <td className="w-3/12">{item.job_description?.slice(0, 300)}</td>
+                <td className="w-2/12">{item.json_response?.tech_stack?.join(", ")}</td>
+                <td className="w-1/12">{item.json_response?.location}</td>
               </tr>
             );
           })}
