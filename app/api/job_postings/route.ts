c8ea041b31cce0cc5654f1211e9f3011c5040788
michael-bocim <mtbocim@gmail.com>
2023-11-16 12:56:24 -0700

    Prisma update and intial searching test

--- app/api/job_postings/route.ts
+++ app/api/job_postings/route.ts
@@ -1,12 +1,36 @@
 import prisma from '@/app/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
-
+import JobPostingsInterface from '@/app/interfaces/JobPostingsInterface';
 export async function GET() {
-    let results = await prisma.job_postings.findMany({where:{
-        json_response:{
-            path: ['apply'],
-            equals:'True'
-        }
-    }})
+    const results = process.env.DATABASE_URL
+    ? ((await prisma.job_postings.findMany({
+        where: {
+          OR: [
+            {
+              job_description: {
+                search: "%react%",
+              },
+            },
+            {
+              AND: [
+                {
+                  json_response: {
+                    path: ["apply"],
+                    equals: "True",
+                  },
+                },
+                {
+                  json_response: {
+                    path: ["tech_stack"],
+                    array_contains: ["%react%"],
+                    
+                  },
+                },
+              ],
+            },
+          ],
+        },
+      })) as Array<JobPostingsInterface>)
+    : [];
     return NextResponse.json(results)
 }
