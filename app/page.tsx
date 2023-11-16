56dca3305785e98aa767ddc743af0d9baeec4d8e
Tanya Shylock <tourtanya@gmail.com>
2023-11-16 14:29:59 -0700

    push current code

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import prisma from "@/app/lib/prisma";
 import JobPostingsInterface from "@/app/interfaces/JobPostingsInterface";
 import JobTable from "./components/JobTable";
+
 export default async function Home() {
   const results = process.env.DATABASE_URL
     ? ((await prisma.job_postings.findMany({
