f53076dc5642973888cc622c8c9304723a40f7aa
Crynowecer0 <crynowecer11@gmail.com>
2023-11-16 19:37:07 -0500

    progress on converting table to MUI datagrid

--- app/components/JobTable2.tsx
+++ app/components/JobTable2.tsx
@@ -8,34 +8,40 @@ import TableHead from '@mui/material/TableHead';
 import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 
+import JobPostingsInterface from "../interfaces/JobPostingsInterface";
+
 interface Column {
-  id: 'Job Title' | 'Company' | 'Description' | 'Tech Stack' | 'Location';
+  id: 'job_title' | 'company' | 'description' | 'tech_stack' | 'location';
   label: string;
   minWidth?: number;
   align?: 'right';
   format?: (value: number) => string;
 }
 
+interface JobTableProps {
+  descriptions: Array<JobPostingsInterface>;
+}
+
 const columns: readonly Column[] = [
-  { id: 'name', label: 'Name', minWidth: 170 },
-  { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
+  { id: 'job_title', label: 'Job Title', minWidth: 170 },
+  { id: 'company', label: 'Company Name', minWidth: 100 },
   {
-    id: 'population',
-    label: 'Population',
+    id: 'description',
+    label: 'Description',
     minWidth: 170,
     align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'size',
-    label: 'Size\u00a0(km\u00b2)',
+    id: 'tech_stack',
+    label: 'tech_stack',
     minWidth: 170,
     align: 'right',
     format: (value: number) => value.toLocaleString('en-US'),
   },
   {
-    id: 'density',
-    label: 'Density',
+    id: 'location',
+    label: 'Location',
     minWidth: 170,
     align: 'right',
     format: (value: number) => value.toFixed(2),
@@ -60,25 +66,9 @@ function createData(
   return { name, code, population, size, density };
 }
 
-const rows = [
-  createData('India', 'IN', 1324171354, 3287263),
-  createData('China', 'CN', 1403500365, 9596961),
-  createData('Italy', 'IT', 60483973, 301340),
-  createData('United States', 'US', 327167434, 9833520),
-  createData('Canada', 'CA', 37602103, 9984670),
-  createData('Australia', 'AU', 25475400, 7692024),
-  createData('Germany', 'DE', 83019200, 357578),
-  createData('Ireland', 'IE', 4857000, 70273),
-  createData('Mexico', 'MX', 126577691, 1972550),
-  createData('Japan', 'JP', 126317000, 377973),
-  createData('France', 'FR', 67022000, 640679),
-  createData('United Kingdom', 'GB', 67545757, 242495),
-  createData('Russia', 'RU', 146793744, 17098246),
-  createData('Nigeria', 'NG', 200962417, 923768),
-  createData('Brazil', 'BR', 210147125, 8515767),
-];
 
-export default function StickyHeadTable() {
+
+export default function StickyHeadTable({descriptions:}) {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
