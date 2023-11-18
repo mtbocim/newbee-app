19dc405af51d25d64e47986646cff8c55f6853c9
michael-bocim <mtbocim@gmail.com>
2023-11-17 20:29:09 -0700

    Setting route to receive params

--- app/api/job_postings/route.ts
+++ app/api/job_postings/route.ts
@@ -1,36 +1,40 @@
 import prisma from '@/app/lib/prisma'
 import { NextRequest, NextResponse } from 'next/server'
 import JobPostingsInterface from '@/app/interfaces/JobPostingsInterface';
-export async function GET() {
+export async function GET(request: NextRequest) {
+    
+    const searchParams = request.nextUrl.searchParams
+    const query = searchParams.get('query')
+
     const results = process.env.DATABASE_URL
-    ? ((await prisma.job_postings.findMany({
-        where: {
-          OR: [
-            {
-              job_description: {
-                search: "%react%",
-              },
+        ? ((await prisma.job_postings.findMany({
+            where: {
+                OR: [
+                    {
+                        job_description: {
+                            search: `%${query}%`,
+                        },
+                    },
+                    {
+                        AND: [
+                            {
+                                json_response: {
+                                    path: ["apply"],
+                                    equals: "True",
+                                },
+                            },
+                            {
+                                json_response: {
+                                    path: ["tech_stack"],
+                                    array_contains: [`%${query}%`],
+
+                                },
+                            },
+                        ],
+                    },
+                ],
             },
-            {
-              AND: [
-                {
-                  json_response: {
-                    path: ["apply"],
-                    equals: "True",
-                  },
-                },
-                {
-                  json_response: {
-                    path: ["tech_stack"],
-                    array_contains: ["%react%"],
-                    
-                  },
-                },
-              ],
-            },
-          ],
-        },
-      })) as Array<JobPostingsInterface>)
-    : [];
+        })) as Array<JobPostingsInterface>)
+        : [];
     return NextResponse.json(results)
 }
