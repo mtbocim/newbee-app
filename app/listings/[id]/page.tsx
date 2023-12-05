377e98aa004f5730112944a033eb7f7337f65c0d
michael-bocim <mtbocim@gmail.com>
2023-12-05 12:00:56 -0700

    Started code before branch switch

--- app/listings/[id]/page.tsx
+++ app/listings/[id]/page.tsx
@@ -1,46 +1,123 @@
-import prisma from "@/app/lib/prisma";
-import JobListingsInterface from "@/app/interfaces/JobListingsInterface";
-import { redirect } from "next/navigation";
+import prisma from '@/app/lib/prisma';
+import JobListingsInterface from '@/app/interfaces/JobListingsInterface';
+import { Typography } from '@mui/material';
+import { redirect } from 'next/navigation';
+import Header from '@/app/components/Header';
+import Image from 'next/image';
+import DescriptionOutlinedIcon from '@mui/icons-material/DescriptionOutlined';
+import MonetizationOnOutlinedIcon from '@mui/icons-material/MonetizationOnOutlined';
+import SettingsApplicationsOutlinedIcon from '@mui/icons-material/SettingsApplicationsOutlined';
+import WorkOutlineOutlinedIcon from '@mui/icons-material/WorkOutlineOutlined';
+import BusinessOutlinedIcon from '@mui/icons-material/BusinessOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import CreateOutlinedIcon from '@mui/icons-material/CreateOutlined';
+import FormatListBulletedSharpIcon from '@mui/icons-material/FormatListBulletedSharp';
 
 export default async function Listing({ params }: { params: { id: string } }) {
-  const id = Number(params.id);
+    const id = Number(params.id);
 
-  if (isNaN(id)) {
-    redirect("/listings");
-  }
-
-  try {
-    const result = (await prisma.job_postings.findFirst({
-      where: {
-        id: id,
-      },
-    })) as JobListingsInterface;
-
-    if (result === null) {
-      //Add handling here for if result comes back empty.
-      //Back to listings page? No result found error with button?
-      return (
-        <div>
-          No such job found
-          <br />
-          <a href="/listings" style={{ color: "blue" }}>
-            Back to listings
-          </a>
-        </div>
-      );
+    if (isNaN(id)) {
+        redirect('/listings');
     }
 
-    return (
-      <div>
-        {/* Job listing components here */}
-        Hi :-)
-        <br />
-        {
-          result?.job_title /*Placeholder line to see listing is correctly queried*/
+    try {
+        const result = (await prisma.job_postings.findFirst({
+            where: {
+                id: id,
+            },
+        })) as JobListingsInterface;
+
+        if (result === null) {
+            //Add handling here for if result comes back empty.
+            //Back to listings page? No result found error with button?
+            return (
+                <div>
+                    No such job found
+                    <br />
+                    <a
+                        href="/listings"
+                        style={{ color: 'blue' }}
+                    >
+                        Back to listings
+                    </a>
+                </div>
+            );
         }
-      </div>
-    );
-  } catch {
-    // Not sure where to send people or what to display if query fails at the moment
-  }
+        console.log('');
+        return (
+            <>
+                <Header pageName="Job Details" />
+                <div className="job-details">
+                    {/* Job listing components here */}
+                    <Typography variant="h5">
+                        <WorkOutlineOutlinedIcon style={{ color: '#0c4a6e' }} />{' '}
+                        {result.job_title}
+                    </Typography>
+                    <Typography variant="h6">
+                        {' '}
+                        <BusinessOutlinedIcon
+                            style={{ color: '#0c4a6e' }}
+                        />{' '}
+                        {result.company_name}
+                    </Typography>
+                    <Typography variant="h6">
+                        {' '}
+                        <LocationOnOutlinedIcon
+                            style={{ color: '#0c4a6e' }}
+                        />{' '}
+                        {result.json_response.location}
+                    </Typography>
+                    <br />
+                    <Typography variant="h6">
+                        <MonetizationOnOutlinedIcon
+                            style={{ color: '#0c4a6e' }}
+                        />{' '}
+                        Salary: {result.json_response.salary}
+                    </Typography>
+                    {/* NOTE:should tech stack ever be completly empty? */}
+                    <Typography variant="h6">
+                        <SettingsApplicationsOutlinedIcon
+                            style={{ color: '#0c4a6e' }}
+                        />{' '}
+                        Tech Stack:{' '}
+                        {result.json_response.tech_stack?.join(', ') ||
+                            'not provided'}
+                    </Typography>
+                    <br />
+                    <div>
+                        <Typography variant="h6">Job Summary:</Typography>
+                        {result.json_response.summary}
+                    </div>
+                    <div>
+                        <br />
+                        {/* <DescriptionOutlinedIcon
+            style={{ color: "#0c4a6e" }} />{" "} */}
+                        <Typography variant="h6">The Buzz:</Typography>
+                        {result.json_response.reasoning}
+                    </div>
+                    <br />
+                    <a
+                        href={result.job_url}
+                        target="_blank"
+                    >
+                        <button className="btn btn-primary">
+                            <CreateOutlinedIcon />
+                            apply here
+                        </button>
+                    </a>
+                    <br />
+                    <br />
+                    <a href="/">
+                        <button className="btn btn-ghost">
+                            <FormatListBulletedSharpIcon />
+                            All listings
+                        </button>
+                    </a>
+                </div>
+            </>
+        );
+    } catch {
+        console.log('error');
+        // Not sure where to send people or what to display if query fails at the moment
+    }
 }
--- app/listings/page.tsx
+++ app/listings/page.tsx
@@ -1,54 +1,54 @@
-import prisma from "@/app/lib/prisma";
-import JobListingsInterface from "@/app/interfaces/JobListingsInterface";
-import JobTable from "@/app/components/JobTable";
-import SearchBar from "../components/SearchBar";
-import HomeHero from "../components/HomeHero";
+import prisma from '@/app/lib/prisma';
+import JobListingsInterface from '@/app/interfaces/JobListingsInterface';
+import JobTable from '@/app/components/JobTable';
+import SearchBar from '../components/SearchBar';
+import HomeHero from '../components/HomeHero';
 
 export default async function Listings({
-  searchParams,
+    searchParams,
 }: {
-  searchParams: { [key: string]: string | string[] | undefined };
+    searchParams: { [key: string]: string | string[] | undefined };
 }) {
-  const query = searchParams?.q as string || "";
-  const queryParse = query.split(" ").join(" & ")
-  const results = process.env.DATABASE_URL
-    ? ((await prisma.job_postings.findMany({
-        where: {
-          AND: [
-            {
-              json_response: {
-                path: ["apply"],
-                equals: "True",
+    const query = (searchParams?.q as string) || '';
+    const queryParse = query.split(' ').join(' & ');
+    const results = process.env.DATABASE_URL
+        ? ((await prisma.job_postings.findMany({
+              where: {
+                  AND: [
+                      {
+                          json_response: {
+                              path: ['apply'],
+                              equals: 'True',
+                          },
+                      },
+                      {
+                          OR: [
+                              queryParse
+                                  ? {
+                                        job_description: {
+                                            search: `%${queryParse}%`,
+                                        },
+                                    }
+                                  : {},
+                              queryParse
+                                  ? {
+                                        json_response: {
+                                            path: ['tech_stack'],
+                                            array_contains: [`%${queryParse}%`],
+                                        },
+                                    }
+                                  : {},
+                          ],
+                      },
+                  ],
               },
-            },
-            {
-              OR: [
-                queryParse
-                  ? {
-                      job_description: {
-                        search: `%${queryParse}%`,
-                      },
-                    }
-                  : {},
-                queryParse
-                  ? {
-                      json_response: {
-                        path: ["tech_stack"],
-                        array_contains: [`%${queryParse}%`],
-                      },
-                    }
-                  : {},
-              ],
-            },
-          ],
-        },
-      })) as Array<JobListingsInterface>)
-    : [];
-  return (
-    <>
-      <HomeHero />
-      <SearchBar/>
-      <JobTable descriptions={results} />
-    </>
-  );
+          })) as Array<JobListingsInterface>)
+        : [];
+    return (
+        <>
+            <HomeHero />
+            <SearchBar />
+            <JobTable descriptions={results} />
+        </>
+    );
 }
